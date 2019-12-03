570561429188dd26531d1dd077d0f2b3b277448f
Yuxuan Zhang <yz3630@columbia.edu>
2019-12-03 00:13:18 +0000

    Add a view lists all squirrel sightings.

--- track/views.py
+++ track/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import Squirrels
+from .forms import SightingForm
+
+
+
+
+def home(request):
+    sighting_list = Squirrels.objects.all()
+    return render(request, 'track/home.html', locals())
 
 # Create your views here.
