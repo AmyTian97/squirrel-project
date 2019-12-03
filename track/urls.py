a18af4908055aef4cdc28783e743e430bf2916b4
Yumeng Tian <yt2680@columbia.edu>
2019-12-03 21:31:11 +0000

    Add url for update_sighitngs view

--- track/urls.py
+++ track/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
         path('sightings/',views.home,name='home'),
         path('sightings/add',views.add_sightings,name='add-sightings'),
         path('sightings/<str:unique_squirrel_id>',views.sighting_detail,name='sighting_detail'),
+        path('sightings/<str:unique_squirrel_id>/update',views.update_sightings,name='update_sightings'),
         ]
