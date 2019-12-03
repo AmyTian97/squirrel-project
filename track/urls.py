293ad5e7a160b436990b11103a1a42ffd1d8cce4
Yuxuan Zhang <yz3630@columbia.edu>
2019-12-03 16:50:19 +0000

    Add path for the add page.

--- track/urls.py
+++ track/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
         path('sightings/',views.home,name='home'),
+        path('sightings/add',views.add_sightings,name='add-sightings'),
         path('sightings/<str:unique_squirrel_id>',views.sighting_detail,name='sighting_detail'),
         ]
