2d4732cb7503c712977d09d278ad0783ef5b6e8f
Yuxuan Zhang <yz3630@columbia.edu>
2019-12-04 06:01:58 +0000

    Add path for map.

--- track/urls.py
+++ track/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+        path('map/',views.map),
         path('sightings/',views.home,name='home'),
         path('sightings/add',views.add_sightings,name='add-sightings'),
         path('sightings/<str:unique_squirrel_id>',views.sighting_detail,name='sighting_detail'),
