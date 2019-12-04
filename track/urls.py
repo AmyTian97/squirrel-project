33da0d034f548a8a2d8e87dc43b1b55a8c18f599
Yumeng Tian <yt2680@columbia.edu>
2019-12-04 15:11:28 +0000

    Add a url path for deleting view

--- track/urls.py
+++ track/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
         path('map/',views.map),
         path('sightings/',views.home,name='home'),
-        path('sightings/add',views.add_sightings,name='add-sightings'),
-        path('sightings/<str:unique_squirrel_id>',views.sighting_detail,name='sighting_detail'),
-        path('sightings/<str:unique_squirrel_id>/update',views.update_sightings,name='update_sightings'),
+        path('sightings/add/',views.add_sightings,name='add-sightings'),
+        path('sightings/<str:unique_squirrel_id>/',views.sighting_detail,name='sighting_detail'),
+        path('sightings/<str:unique_squirrel_id>/update/',views.update_sightings,name='update_sightings'),
+        path('sightings/<str:unique_squirrel_id>/delete/',views.delete_sightings,name='delete_sightings'),
         ]
