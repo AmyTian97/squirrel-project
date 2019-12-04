6a06ee2e26b8856c00a2a13d49eaa6549e5067d0
Yuxuan Zhang <yz3630@columbia.edu>
2019-12-04 18:05:04 +0000

    Add path for stat page.

--- track/urls.py
+++ track/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
         path('map/',views.map),
         path('sightings/',views.home,name='home'),
+        path('sightings/stat/',views.stat,name='stat'),
         path('sightings/add/',views.add_sightings,name='add-sightings'),
         path('sightings/<str:unique_squirrel_id>/',views.sighting_detail,name='sighting_detail'),
         path('sightings/<str:unique_squirrel_id>/update/',views.update_sightings,name='update_sightings'),
