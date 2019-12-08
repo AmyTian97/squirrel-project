e7df619b6e88160406a9cc17e159e5be7fd77147
RannieZ <yz3630@columbia.edu>
2019-12-07 23:47:57 -0500

    Update urls.py

--- track/urls.py
+++ track/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-        path('map/',views.map),
-        path('sightings/',views.home,name='home'),
-        path('sightings/stat/',views.stat,name='stat'),
-        path('sightings/add/',views.add_sightings,name='add-sightings'),
-        path('sightings/<str:unique_squirrel_id>/',views.sighting_detail,name='sighting_detail'),
-        path('sightings/<str:unique_squirrel_id>/update/',views.update_sightings,name='update_sightings'),
-        path('sightings/<str:unique_squirrel_id>/delete/',views.delete_sightings,name='delete_sightings'),
+        path('map/', views.map, name='map'),
+        path('sightings/', views.home, name='home'),
+        path('sightings/stat/', views.stat, name='stat'),
+        path('sightings/add/', views.add_sightings, name='add-sightings'),
+        path('sightings/<str:unique_squirrel_id>/', views.sighting_detail, name='sighting_detail'),
+        path('sightings/<str:unique_squirrel_id>/update/', views.update_sightings, name='update_sightings'),
+        path('sightings/<str:unique_squirrel_id>/delete/', views.delete_sightings, name='delete_sightings'),
         ]
